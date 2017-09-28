69f9d05cf1dfed67d5aa7b5760c61f99c15e4eb8
Parker Catalano <catalano.work@gmail.com>
2017-09-28 17:59:07 +0000

    automatically backed up by learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,5 +1,5 @@
 def current_age_for_birth_year(year)
   age_of_person = 2017 - year;
-  current_age_for_birth_year = age_of_person;
+  age_of_person = current_age_for_birth_year ;
   print age_of_person;
 end
